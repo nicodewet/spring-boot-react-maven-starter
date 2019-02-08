51ca33addcffc78b96e22eb48bcf2967f7a82029
Nico de Wet <nico@nicodewet.com>
2019-02-08 13:49:50 +1300

    Adding Employee and EmployeeList components
    CORS fix still required

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BeerList from './BeerList';
+import EmployeeList from './EmployeeList';
 
 class App extends Component {
-
-    render() {
-
-      return (
-          <div className="App">
-              <header className="App-header">
-                  <img src={logo} className="App-logo" alt="logo" />
-                  <h1 className="App-title">Welcome to React</h1>
-              </header>
-              <BeerList/>
-          </div>
-      );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <BeerList />
+        <EmployeeList />
+      </div>
+    );
   }
 }
 
---
+++ client/src/EmployeeList.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Employee from './Employee';
+
+class EmployeeList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: false, employees: [] };
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+
+    fetch('http://localhost:8080/api/employees')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          employees: data._embedded.employees,
+          isLoading: false
+        });
+      });
+  }
+
+  render() {
+    const { employees, isLoading } = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    const employeesRows = employees.map(employee => (
+      <Employee key={employee._links.self.href} employee={employee} />
+    ));
+
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Description</th>
+          </tr>
+          {employeesRows}
+        </tbody>
+      </table>
+    );
+  }
+}
+export default EmployeeList;
