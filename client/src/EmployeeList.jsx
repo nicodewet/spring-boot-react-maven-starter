cb4a2bcb8676a1988cec647f9af6351f7b19898f
Nico de Wet <nico@nicodewet.com>
2019-02-12 10:02:20 +1300

    Include pageSize in EmployeeList.jsx. Last commit before refactoring messy spaghetti promise based code.

--- client/src/EmployeeList.jsx
+++ client/src/EmployeeList.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import Employee from './Employee';
 
-const root = '/api';
+const halApiRoot = '/api';
 const fetchHeaders = { headers: { 'Content-Type': 'application/hal+json' } };
 const genericNetworkResponseNotOkMessage = 'Network response was not ok.';
+const pageSize = 2;
 
 class EmployeeList extends Component {
   constructor(props) {
@@ -14,7 +15,7 @@ class EmployeeList extends Component {
   componentDidMount() {
     this.setState({ isLoading: true });
 
-    fetch(root, fetchHeaders)
+    fetch(halApiRoot, fetchHeaders)
       .then(function(response) {
         if (response.ok) {
           return response.json();
@@ -28,26 +29,35 @@ class EmployeeList extends Component {
         ) {
           let employeesContainedLinkHref =
             halRootDocumentData._links['employees'].href;
-          console.log(employeesContainedLinkHref);
+
           let res = employeesContainedLinkHref.split('{?page,size,sort}');
-          // this only works if the page size is less than the total number of employees
-          return fetch(res[0], fetchHeaders);
+          let employeesLink = res[0];
+          // TODO save employeesLink in state
+          let pagedEmployeesUrl = employeesLink + '?size=' + pageSize;
+          return fetch(pagedEmployeesUrl, fetchHeaders);
         } else {
           throw new Error(
             'HAL API root object may not be a Resource Object as it has no _links or contained employee resource links.'
           );
         }
       })
-      .then(function(response) {
-        if (response.ok) {
-          return response.json();
+      .then(function(pagedEmployeesFetchResponse) {
+        if (pagedEmployeesFetchResponse.ok) {
+          return pagedEmployeesFetchResponse.json();
         }
         throw new Error(genericNetworkResponseNotOkMessage);
       })
-      .then(data => {
-        console.log(data);
+      .then(pagedEmployeeResponseData => {
+        console.log(pagedEmployeeResponseData);
+
+        if (pagedEmployeeResponseData.page !== undefined) {
+          // this can be used for validation and user feedback and driving logic:
+          // {size: 2, totalElements: 5, totalPages: 3, number: 0}
+          // the _links can be saved for fetching we need to
+        }
+
         this.setState({
-          employees: data._embedded.employees,
+          employees: pagedEmployeeResponseData._embedded.employees,
           isLoading: false
         });
       })
