58bffbbfa5d3a8be2e1a74c9714e5659589f402d
Nico de Wet <nico@nicodewet.com>
2019-02-21 14:24:41 +1300

    finished basic test case

--- client/src/EmployeeList.test.js
+++ client/src/EmployeeList.test.js
@@ -10,9 +10,13 @@ import fetchMock from 'fetch-mock';
  *
  * The reasoning behind this caveat is that While Jest provides browser globals such as window thanks to jsdom,
  * they are only approximations of the real browser behavior.
+ *
+ * READ this page if you are unfamiliar with react-test-renderer:
+ *
+ * https://reactjs.org/docs/test-renderer.html
  */
 describe('EmployeeList component', () => {
-  it('shows a list of employees', async () => {
+  it('state remains in loading when erroneous response sent from server', async () => {
     fetchMock.get('/api', {});
 
     // Create a TestRenderer instance with the passed React element. It doesn’t use the real DOM, but it still
@@ -21,8 +25,17 @@ describe('EmployeeList component', () => {
     // Return the instance corresponding to the root element, if available. This will not work if the root
     // element is a function component because they don’t have instances.
     const instance = component.getInstance();
+
     await instance.componentDidMount();
-    //
-    console.log(instance.state); // << HERE IS THE SNITCH!
+
+    // When testing React components we shouldn't care about the internal state. We're interested in testing what the
+    // user should see. So, the two assertions below are INCORRECT but I'm leaving them here as an example of what not
+    // to do.
+    expect(instance.state.isLoading === true);
+    expect(instance.state.employees === []);
+
+    // Now lets do things the CORRECT way, that is testing what the user should see.
+    const testInstance = component.root;
+    expect(testInstance.findByType('p').children).toEqual(['Loading...']);
   });
 });
